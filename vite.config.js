571e66ecbf2fdc4dd7dc1d6c899d423b61386292
cyue0007 <betty0931311181@gmail.com>
2025-04-16 23:19:46 +0800

    add:在vite.config.js設定 GitHub Pages 的相對路徑

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/SuatHiongTing/',  // 設定 GitHub Pages 的相對路徑
   plugins: [vue()],
   resolve: {
     alias: {
