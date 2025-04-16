33086fc6a71e6218c70ff5981eb3b38ecff558b3
cyue0007 <betty0931311181@gmail.com>
2025-04-16 23:54:11 +0800

    add:加上 base path

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue' // ✅ 把這行改掉！
+import Home from '@/views/Home.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/SuatHiongTing/'),  // 加上 base path
   routes
 })
 
