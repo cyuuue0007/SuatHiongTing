3ddfc6693eb3c910364d335c43a83fd57d72b701
cyue0007 <betty0931311181@gmail.com>
2025-04-17 00:33:37 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/SuatHiongTing/',  // 設定 GitHub Pages 的相對路徑
   plugins: [vue()],
   resolve: {
     alias: {
@@ -17,6 +16,9 @@ export default defineConfig({
       scss: {
         additionalData: `@use "@/assets/styles/variables.scss" as *;`,
       },
+  build: {
+    outDir: 'dist'
+  }    
     },
   },
 })
